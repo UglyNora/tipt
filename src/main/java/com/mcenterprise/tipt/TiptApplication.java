5b8db5732fc8bbd3d75a0f921152e47ff05e5e3b
uglynora <mangos8@yahoo.com>
2020-10-20 10:45:31 -0400

    Initialized Spring Boot project

---
+++ src/main/java/com/mcenterprise/tipt/TiptApplication.java
@@ -0,0 +1,13 @@
+package com.mcenterprise.tipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcenterprise/tipt/TiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcenterprise.tipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
