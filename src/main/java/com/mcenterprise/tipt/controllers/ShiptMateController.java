997bf6dc6a87278113a2e5f04b5a07368802bf2c
uglynora <mangos8@yahoo.com>
2020-11-06 21:01:59 -0500

    Configured user signup.

--- src/main/java/com/mcenterprise/tipt/controllers/ShiptMateController.java
+++ src/main/java/com/mcenterprise/tipt/controllers/ShiptMateController.java
@@ -1,14 +1,40 @@
 package com.mcenterprise.tipt.controllers;
 
+
+import com.mcenterprise.tipt.models.ShiptMate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("shiptMate")
 public class ShiptMateController {
-    @GetMapping("ShiptMate")
-    @ResponseBody
-    public String hello(){
-        return "Page displayed when messaging another member.";
-    };
+
+    @GetMapping("/addShiptMate")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title", "Create ShiptMate");
+        model.addAttribute("shiptMate", new ShiptMate());
+        return "shiptMate/addShiptMate";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute ShiptMate shiptMate, String verify) {
+        model.addAttribute("shiptMate", shiptMate);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", shiptMate.getUsername());
+        model.addAttribute("email", shiptMate.getEmail());
+        if (shiptMate.getPassword().equals(verify)) {
+            return "shiptMate/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "shiptMate/addShiptMate";
+        }
+
+    }
+
+
 }
--- src/main/java/com/mcenterprise/tipt/models/ShiptMate.java
+++ src/main/java/com/mcenterprise/tipt/models/ShiptMate.java
@@ -1,4 +1,93 @@
 package com.mcenterprise.tipt.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 public class ShiptMate {
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank(message = "Please provide a username!")
+    @Size(min = 5, max= 15)
+    private String username;
+
+    @Email(message = "Invalid email!")
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 6, message = "Password must be at least 6 characters!")
+    private String password;
+    @NotNull(message= "Passwords Do Not Match!")
+    private String verifyPassword;
+
+
+
+    public ShiptMate() {
+
+    }
+
+    public ShiptMate(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+
+        this.password = password;
+        checkPassword();
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if((getPassword() != null) && (getVerifyPassword() != null) && (getPassword() != getVerifyPassword())) {
+            verifyPassword = null;
+        }
+
+    }
 }
+
+
